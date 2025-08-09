00c5ad85de496d2a0948ccc1dc880263270f2917
vh-mendes <vitorhugomendes995@gmail.com>
2025-08-09 13:22:21 -0300

    Implementando a criação de conta, sign-up

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,9 +2,26 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "@/db";
- 
+import * as schema from "@/db";
+
 export const auth = betterAuth({
-        database: drizzleAdapter(db, {
-        provider: "pg", 
-    }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  user: {
+    modelName: "userTable",
+  },
+  session: {
+    modelName: "sessionTable",
+  },
+  account: {
+    modelName: "accountTable",
+  },
+  verification: {
+    modelName: "verificationTable",
+  },
 });
