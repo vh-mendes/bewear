3670b2f229f69fc59014197a6385ae09ac0eb31f
vh-mendes <vitorhugomendes995@gmail.com>
2025-08-09 14:56:51 -0300

    Integrando o sign-in com google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ const Home = async () => {
       <div className="space-y-6">
         <div className="px-5">
           <Image
-            src="/banner-01.png"
+            src="/banner-02.png"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
@@ -45,7 +45,7 @@ const Home = async () => {
 
         <div className="px-5">
           <Image
-            src="/banner-02.png"
+            src="/banner-01.png"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -8,7 +8,7 @@ interface CategorySelectorProps {
 
 const CategorySelector = ({ categories }: CategorySelectorProps) => {
   return (
-    <div className="rounded-3xl bg-[#F4EFFF] p-6">
+    <div className="rounded-3xl bg-[#c5dcff] p-6">
       <div className="grid grid-cols-2 gap-3">
         {categories.map((category) => (
           <Button
