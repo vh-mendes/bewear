6584a025b5d34360461852178304ba4bc9bf77c7
vh-mendes <vitorhugomendes995@gmail.com>
2025-08-09 00:14:49 -0300

    feat: Inicializando o projeto do e-commerce, a Bewear

---
+++ src/app/product/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Product = () => {
+  return (
+    <div>
+        <Button>Nada deu certo</Button>
+      <h1 className="px-5 my-1">Product Page</h1>
+      <p>Welcome to the product page!</p>
+    </div>
+  );
+}
+
+export default Product;
