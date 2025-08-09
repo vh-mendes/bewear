a58f6dd0d71593deb14909f7e6e1b345a31fd904
vh-mendes <vitorhugomendes995@gmail.com>
2025-08-09 14:32:37 -0300

    Adicionando o footer

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent w-full gap-1 p-8">
+      <p className="text-xs font-medium">© 2025 Copyright BEWEAR</p>
+      <p className="text-muted-foreground text-xs font-medium">
+        Todos os direitos reservados<br></br>Desenvolvido por Vítor Mendes
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
